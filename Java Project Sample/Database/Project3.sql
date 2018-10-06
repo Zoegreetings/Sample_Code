a8e0f4d68defd58b20705b49ddf64651df7e1e75
Zoegreetings <zenggreetings@gmail.com>
2018-10-05 23:18:32 -0400

    Update Project3.sql

--- Java Project Sample/Database/Project3.sql
+++ Java Project Sample/Database/Project3.sql
@@ -1,22 +1,7 @@
--- phpMyAdmin SQL Dump
--- version 3.4.10.1deb1
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Apr 22, 2016 at 10:40 AM
--- Server version: 5.5.35
--- PHP Version: 5.3.10-1ubuntu3.10
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
 -- Database: `Project3`
 --
 
